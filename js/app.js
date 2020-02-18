733bc472c6f69f9cb4d61dd51e4d0fc5c5d9eea7
mrsmarah <marahabdallah91@gmail.com>
2020-02-18 10:35:01 +0200

    edit

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-"use strict";
-var username = prompt("Hello, it's Marah! What's your name ?");
-alert("Welcome to my page "+ username +" ! ");
-console.log("Welcome to my page "+ username +" ! ");
+'use strict';
+var username = prompt('Hello, it\'s Marah! What\'s your name ?');
+alert('Welcome to my page '+ username +' ! ');
+//console.log('Welcome to my page '+ username +' ! ');
 
-var favColor = prompt("Let's see if you can guess these questions: Does pink is my favorite color ?");
+var favColor = prompt('Let\'s see if you can guess these questions: Does pink is my favorite color ?');
 if (favColor.toLocaleLowerCase() === "yes" || favColor.toLocaleLowerCase() === "y") {
     alert("Nope "+ username + ", White is my favorite.");
 }
@@ -13,50 +13,55 @@ else if (favColor.toLocaleLowerCase() === "no" || favColor.toLocaleLowerCase() =
 else {
     alert("sorry, you have to answer with yes or no.");
 }
+//console.log(favColor);
 
-var coding = prompt("Do you think that I have a background in coding ?");
+var coding = prompt('Do you think that I have a background in coding ?');
 if (coding.toLocaleLowerCase() === "yes" || coding.toLocaleLowerCase() === "y") {
-    alert("Wrong, I have zero backgroung about coding.");
+    alert('Wrong, I have zero backgroung about coding.');
 }
 else if (coding.toLocaleLowerCase() ==="no" || coding.toLocaleLowerCase() === "n"){
-    alert("Right answer "+ username + " I have zero backgroung about coding.");
+    alert('Right answer '+ username + ' I have zero backgroung about coding.');
 }
 else {
-    alert("sorry, you have to answer with yes/no or y/n.");
+    alert('sorry, you have to answer with yes/no or y/n.');
 }
+//console.log(coding);
 
-var pets = prompt("Do you think I love pets ?");
+var pets = prompt('Do you think I love pets ?');
 if (pets.toLocaleLowerCase() === "yes" || pets.toLocaleLowerCase() === "y"){
-    alert("absolutely! I love cats.");
+    alert('absolutely! I love cats.');
 }
 else if (pets.toLocaleLowerCase() === "no" || pets.toLocaleLowerCase() === "n"){
-    alert("wrong answer "+ username + ", I do love pets.");
+    alert('wrong answer '+ username + ', I do love pets.');
 }
 else {
-    alert("sorry, you have to answer with yes/no or y/n.");
+    alert('sorry, you have to answer with yes/no or y/n.');
 }
+//console.log(pets);
 
-var season = prompt("Does summer is my favorite season ?");
+var season = prompt('Does summer is my favorite season ?');
 if (season.toLocaleLowerCase() === "yes" || season.toLocaleLowerCase() === "y") {
-    alert("Right answer "+ username + " !");
+    alert('Right answer '+ username + ' !');
 }
 else if (season.toLocaleLowerCase() ==="no" || season.toLocaleLowerCase() === "n"){
-    alert("wrong answer "+ username + ", it is !");
+    alert('wrong answer '+ username + ', it is !');
 }
 else {
-    alert("sorry, you have to answer with yes/no or y/n.");
+    alert('sorry, you have to answer with yes/no or y/n.');
 }
+//console.log(season);
 
-var talent = prompt("Do you think I have a singing talent ?");
+var talent = prompt('Do you think I have a singing talent ?');
 if (talent.toLocaleLowerCase() === "yes" || talent.toLocaleLowerCase() === "y"){
-    alert("Wrong answer " + username + ", I have a horrible voice!" );
+    alert('Wrong answer ' + username + ', I have a horrible voice!' );
 }
 else if (talent.toLocaleLowerCase() ==="no" || talent.toLocaleLowerCase() === "n"){
-    alert("Absolutely Right!");
+    alert('Absolutely Right!');
 }
 else {
-    alert("sorry, you have to answer with yes/no or y/n.");
+    alert('sorry, you have to answer with yes/no or y/n.');
 }
+//console.log(talent);
 
 
 

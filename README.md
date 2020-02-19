4580e1e9f6c281aa74e281889d31a440fe81d712
mrsmarah <marahabdallah91@gmail.com>
2020-02-19 03:47:25 +0200

    add

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # About-me
 Hello, it's Marah Joudeh's page 
+
+**This website is all about me** 
+
+The user will get to know me better by the guessing quiz that i made.
+
+Also the user can get a final score that indicates how much he knows me, So let's have fun !
--- js/app.js
+++ js/app.js
@@ -1,26 +1,31 @@
 'use strict';
+
+var score = 0 ;
+
 var username = prompt('Hello, it\'s Marah! What\'s your name ?');
 alert('Welcome to my page '+ username +' ! ');
 //console.log('Welcome to my page '+ username +' ! ');
 
-var favColor = prompt('Let\'s see if you can guess these questions: Does pink is my favorite color ?');
+var favColor = prompt('Let\'s see how much you know about me! Does pink is my favorite color ?');
 if (favColor.toLocaleLowerCase() === "yes" || favColor.toLocaleLowerCase() === "y") {
-    alert("Nope "+ username + ", White is my favorite.");
+    alert("Nope "+ username + ", White is my favorite.");   
 }
 else if (favColor.toLocaleLowerCase() === "no" || favColor.toLocaleLowerCase() === "n"){
     alert("you're right "+ username +", White is my favorite.");
+    score = score + 1 ;
 }
 else {
     alert("sorry, you have to answer with yes or no.");
 }
 //console.log(favColor);
 
-var coding = prompt('Do you think that I have a background in coding ?');
+var coding = prompt('Do you think I had a background in coding before this course ?');
 if (coding.toLocaleLowerCase() === "yes" || coding.toLocaleLowerCase() === "y") {
-    alert('Wrong, I have zero backgroung about coding.');
+    alert('Wrong, I had zero backgroung about coding.');
 }
 else if (coding.toLocaleLowerCase() ==="no" || coding.toLocaleLowerCase() === "n"){
-    alert('Right answer '+ username + ' I have zero backgroung about coding.');
+    alert('Right answer '+ username + ' I had zero backgroung about coding.');
+    score = score + 1 ;
 }
 else {
     alert('sorry, you have to answer with yes/no or y/n.');
@@ -30,6 +35,7 @@ else {
 var pets = prompt('Do you think I love pets ?');
 if (pets.toLocaleLowerCase() === "yes" || pets.toLocaleLowerCase() === "y"){
     alert('absolutely! I love cats.');
+    score = score + 1 ;
 }
 else if (pets.toLocaleLowerCase() === "no" || pets.toLocaleLowerCase() === "n"){
     alert('wrong answer '+ username + ', I do love pets.');
@@ -42,6 +48,7 @@ else {
 var season = prompt('Does summer is my favorite season ?');
 if (season.toLocaleLowerCase() === "yes" || season.toLocaleLowerCase() === "y") {
     alert('Right answer '+ username + ' !');
+    score = score + 1 ;
 }
 else if (season.toLocaleLowerCase() ==="no" || season.toLocaleLowerCase() === "n"){
     alert('wrong answer '+ username + ', it is !');
@@ -57,13 +64,57 @@ if (talent.toLocaleLowerCase() === "yes" || talent.toLocaleLowerCase() === "y"){
 }
 else if (talent.toLocaleLowerCase() ==="no" || talent.toLocaleLowerCase() === "n"){
     alert('Absolutely Right!');
+    score = score + 1 ;
 }
 else {
     alert('sorry, you have to answer with yes/no or y/n.');
 }
-//console.log(talent);
+// console.log(talent);
 
+for( var i = 0 ; i < 4 ; i++ ){
+    var age = prompt('How old Do I look like ?');
+    age = parseInt(age);
+    console.log( typeof age);
+    
+    if (age === 23 ){
+        alert('You\'re Right!');
+        score = score + 1 ;
+        { break; }
+    }
+    if (i == 3){
+        alert('No more attempts! I\'m 23 .');
+    }  
+    else if (age > 23){
+            alert('Too old, try again! *' + (3-i) + ' attempts left!');   
+        }
+    
+    else if (age < 23){
+            alert('Too young, try again! *'+ (3-i) + ' attempts left!');
+        }
+    else {
+            alert('Please enter a number!');
+        }
+          
+}
+// console.log(age);
 
+var city = ['salt','amman','irbid','madaba']
 
+for ( var j = 0 ; j < 6 ; j++ ){
+    var favCity = prompt('What is my Favorite city in jordan ?');
+    if ( favCity.toLocaleLowerCase() === city[j] ){
+        alert('true');
+        score = score + 1 ;
+        {break};
+    }
+    
+    if (j == 5){
+        alert('No more attempts! these are my favorite cities: salt, amman, irbid, madaba.');
+    }
 
-
+    else if (favCity.toLocaleLowerCase() !== city[j] ){
+            alert('Wrong answer; try out another one! *' + (5-j) + ' attempts left!' );
+        }
+     }           
+    
+alert('Your final score out of 7 is : ' + score);
